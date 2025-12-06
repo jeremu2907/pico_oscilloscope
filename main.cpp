e27b30874369ccce288b4b2c70bfbfe0466577d7
jeremu2907 <jeremu2907@gmail.com>
2025-12-06 15:44:42 -0600

    removed magic number and calibrated

--- main.cpp
+++ main.cpp
@@ -14,6 +14,12 @@
 
 int main()
 {
+    constexpr float VOLTAGE_LEVEL_BIAS = 1.5125f;
+    constexpr float FRONTEND_VOLTAGE_DIVIDER_RATIO = (1e3 + 10e3) / 1e3;
+    constexpr float MINIMUN_VOLTAGE_THRESHOLD = 0.005;
+    constexpr float REVERSED_POLARITY_VOLTAGE_BIAS = 0.2;
+    constexpr uint64_t MS_BETWEEN_SSD_1306_UPDATE = 100;
+
 #if IS_WIRELESS
     if (cyw43_arch_init())
     {
@@ -26,35 +32,66 @@ int main()
     Gpio::Base::onboardLedOn();
 
     uint64_t currentTimeMs = to_ms_since_boot(get_absolute_time());
-    Adc::Input gp28A2(26, Adc::Input::GPIO_28_ADC);
+    Adc::Input gp28A2(28, Adc::Input::GPIO_28_ADC);
+    Adc::Input gp27A1(27, Adc::Input::GPIO_27_ADC);
     gpio_pull_down(28);
+    gpio_pull_down(27);
     I2c::Ssd1306 oled(20, 21, i2c0);
 
+    float vChannel0 = 0.0;
+    float vChannel1 = 0.0;
+
     gp28A2.installCallback(
-        [&currentTimeMs, &oled](float voltage)
+        [&currentTimeMs, &oled, &vChannel0](float voltage)
         {
-            float measuredVoltage = 11.0 * 1.05 * (voltage - 1.55613);
-            if(abs(measuredVoltage) < 0.0393566074)
+            float unbiasedVoltage = voltage - VOLTAGE_LEVEL_BIAS;
+            vChannel0 = FRONTEND_VOLTAGE_DIVIDER_RATIO * unbiasedVoltage;
+            if (abs(unbiasedVoltage) < MINIMUN_VOLTAGE_THRESHOLD)
             {
-                measuredVoltage = 0.0;
+                vChannel0 = 0.0;
+            }
+            if(vChannel0 < 0.0)
+            {
+                vChannel0 -= REVERSED_POLARITY_VOLTAGE_BIAS;
             }
 
-            printf("%f\n", measuredVoltage);
+            printf("ch_0: %f\n", vChannel0);
+        });
 
-            uint64_t now = to_ms_since_boot(get_absolute_time());
-            if (now - currentTimeMs > 100)
+    gp27A1.installCallback(
+        [&currentTimeMs, &oled, &vChannel1](float voltage)
+        {
+            float unbiasedVoltage = voltage - VOLTAGE_LEVEL_BIAS;
+            vChannel1 = FRONTEND_VOLTAGE_DIVIDER_RATIO * unbiasedVoltage;
+            if (abs(unbiasedVoltage) < MINIMUN_VOLTAGE_THRESHOLD)
             {
-                std::string s = std::to_string(measuredVoltage);
-                std::string padding = ((measuredVoltage < 0)? "" : " " );
-                s = "\n\nVolts: " + padding + s;
-                oled.clearData();
-                oled.setData(s);
-                oled.writeData();
-                currentTimeMs = now;
+                vChannel1 = 0.0;
             }
+            if(vChannel1 < 0.0)
+            {
+                vChannel1 -= REVERSED_POLARITY_VOLTAGE_BIAS;
+            }
+
+            printf("ch_1: %f\n", vChannel1);
         });
 
     MAIN_LOOP_START
     Adc::Input::runLoop();
+    uint64_t now = to_ms_since_boot(get_absolute_time());
+    if (now - currentTimeMs > MS_BETWEEN_SSD_1306_UPDATE)
+    {
+        std::string vChannel0Str = std::to_string(vChannel0);
+        std::string vChannel1Str = std::to_string(vChannel1);
+        std::string paddingChannel0 = ((vChannel0 < 0) ? "" : " ");
+        std::string paddingChannel1 = ((vChannel1 < 0) ? "" : " ");
+        std::string s = (std::string("\n") +
+                        "ch_0:" + paddingChannel0 + vChannel0Str +
+                        "\n\n" +
+                        "ch_1:" + paddingChannel1 + vChannel1Str);
+        oled.clearData();
+        oled.setData(s);
+        oled.writeData();
+        currentTimeMs = now;
+    }
     MAIN_LOOP_END
 }
